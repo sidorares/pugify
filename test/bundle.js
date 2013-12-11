3bbb038cd182d7f3694874acbabe9fce4b77b040
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2013-12-11 22:16:23 +0100

    Add test for options transfomr

--- test/bundle.js
+++ test/bundle.js
@@ -2,21 +2,29 @@ var test = require('tap').test;
 var browserify = require('browserify');
 var vm = require('vm');
 
-function bundle (file) {
-    test('bundle transform', function (t) {
-        t.plan(1);
-
-        var b = browserify();
-        b.add(__dirname + file);
-        b.transform(__dirname + '/..');
-        b.bundle(function (err, src) {
-            if (err) t.fail(err);
-            testBundle(src, t);
-        });
+test('no options bundle', function(t) {
+    t.plan(1);
+    var b = browserify();
+    b.add(__dirname + '/../example/bar.js');
+    b.transform(__dirname + '/..');
+    b.bundle(function (err, src) {
+        if (err) t.fail(err);
+        testBundle(src, t);
     });
-}
+});
 
-bundle('/../example/bar.js');
+test('options bundle', function(t) {
+    t.plan(1);
+    var b = browserify();
+    b.add(__dirname + '/../example/bar.js');
+    b.transform(require('../index.js').jade({
+        pretty: false
+    }));
+    b.bundle(function (err, src) {
+        if (err) t.fail(err);
+        testBundle(src, t);
+    });
+});
 
 function testBundle(src, t) {
     function log (msg) {
