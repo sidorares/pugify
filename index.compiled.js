38d6bcd9a70610ffe0c18a94d751b5677fb2e5c3
yosh <yoshtalwar@gmail.com>
2016-02-06 22:05:44 +0530

    Rewrite | Creates a basic, working Pug transformer

--- index.compiled.js
+++ index.compiled.js
@@ -1,141 +1,44 @@
-var fs = require('fs');
+'use strict';
 
-var jade = require('jade');
+var pug = require('pug');
+var xtend = require('xtend');
 var through = require('through');
-var transformTools = require('browserify-transform-tools');
 
-var SourceMapGenerator = require('source-map').SourceMapGenerator;
-var convert = require('convert-source-map');
+function pugify(file) {
+  var opts = this || {};
 
-var PREFIX = "var jade = require('jade/lib/runtime.js');\nmodule.exports=";
+  if (!/\.(pug|jade)$/.test(file)) return through();
 
-var defaultJadeOptions = {
-  path: __dirname,
-  compileDebug: true,
-  pretty: true
-};
+  var buffer = '';
 
-function getTransformFn(options) {
-  var key;
-  var opts = {};
-  for (key in defaultJadeOptions) {
-    opts[key] = defaultJadeOptions[key];
-  }
+  return through(function (chunk) {
+    return buffer += chunk.toString();
+  }, function end() {
+    var _this = this;
 
-  options = options || {};
-  for (key in options) {
-    opts[key] = options[key];
-  }
+    var parsed = undefined;
 
-  return function (file) {
-    if (!/\.(pug|jade)$/.test(file)) return through();
-
-    var data = '';
-    return through(write, end);
-
-    function write(buf) {
-      data += buf;
-    }
-    function end() {
-      var _this = this;
-      configData = transformTools.loadTransformConfig('browserify-jade', file, function (err, configData) {
-        if (configData) {
-          var config = configData.config || {};
-          for (key in config) {
-            opts[key] = config[key];
-          }
-        }
-
-        var result = compile(file, data, opts);
-        result.dependencies.forEach(function (dep) {
-          _this.emit('file', dep);
-        });
-        _this.queue(result.body);
-        _this.queue(null);
-      });
-    }
-  };
-}
-
-module.exports = getTransformFn();
-module.exports.jade = getTransformFn;
-module.exports.root = null;
-module.exports.register = register;
-
-function register() {
-  require.extensions[('.pug', '.jade')] = function (module, filename) {
-    var result = compile(filename, fs.readFileSync(filename, 'utf-8'), { compileDebug: true });
-    return module._compile(result.body, filename);
-  };
-}
-
-function replaceMatchWith(match, newContent) {
-  var src = match.input;
-  return src.slice(0, match.index) + newContent + src.slice(match.index + match[0].length);
-}
-
-function withSourceMap(src, compiled, name) {
-
-  //return compiled;
-
-  var compiledLines = compiled.split('\n');
-  var generator = new SourceMapGenerator({ file: name + '.js' });
-
-  compiledLines.forEach(function (l, lineno) {
-    var m = l.match(/^(pug|jade)(_|\.)debug\.unshift\(\{ lineno: ([0-9]+)/);
-    if (m) {
-      var originalLine = Number(m[2]);
-      var generatedLine = lineno + 2;
-
-      if (originalLine > 0) {
-        generator.addMapping({
-          generated: {
-            line: generatedLine,
-            column: 0
-          },
-          source: name,
-          original: {
-            line: originalLine,
-            column: 0
-          }
-        });
-      }
+    try {
+      parsed = pug.compile(buffer, opts);
+    } catch (e) {
+      this.emit('error', e);
+      return this.queue(null);
     }
 
-    var debugRe = /(pug|jade)(_|\.)debug\.(shift|unshift)\([^;]*\);/;
-    var match;
-    while (match = l.match(debugRe)) {
-      l = replaceMatchWith(match, '');
-    }
-    compiledLines[lineno] = l;
+    parsed.dependencies.forEach(function (dep) {
+      return _this.emit('file', dep);
+    });
+
+    this.queue('var jade=require(\'pug-runtime\');module.exports=' + parsed.toString());
+    this.queue(null);
   });
-  generator.setSourceContent(name, src);
-
-  var map = convert.fromJSON(generator.toString());
-  compiledLines.push(map.toComment());
-  return compiledLines.join('\n');
 }
 
-function compile(file, template, options) {
-  options.filename = file;
-  var result;
-  if (jade.compileClientWithDependenciesTracked) {
-    result = jade.compileClientWithDependenciesTracked(template, options);
-  } else if (jade.compileClient) {
-    result = {
-      body: jade.compileClient(template, options).toString(),
-      dependencies: []
-    };
-  } else {
-    // jade < 1.0
-    options.client = true;
-    result = {
-      body: jade.compile(template, options).toString(),
-      dependencies: []
-    };
-  }
-
-  result.body = PREFIX + withSourceMap(template, result.body, file);
-  return result;
-}
+module.exports = function (opts) {
+  return pugify.bind(xtend({
+    // Default options
+    pretty: true,
+    compileDebug: true
+  }, opts));
+};
 
