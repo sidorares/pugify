996b59104e5cf5a1d1bf97037600ce5f0c563b70
yosh <yoshtalwar@gmail.com>
2016-02-07 14:13:32 +0530

    Rewrite | Adds source maps

--- index.compiled.js
+++ index.compiled.js
@@ -1,30 +1,55 @@
 'use strict';
 
-var pug = require('pug');
-var xtend = require('xtend');
-var through = require('through');
-var tools = require('browserify-transform-tools');
+var _pug = require('pug');
+
+var _pug2 = _interopRequireDefault(_pug);
+
+var _xtend = require('xtend');
+
+var _xtend2 = _interopRequireDefault(_xtend);
+
+var _through = require('through');
+
+var _through2 = _interopRequireDefault(_through);
+
+var _convertSourceMap = require('convert-source-map');
+
+var _convertSourceMap2 = _interopRequireDefault(_convertSourceMap);
+
+var _browserifyTransformTools = require('browserify-transform-tools');
+
+var _browserifyTransformTools2 = _interopRequireDefault(_browserifyTransformTools);
+
+var _sourceMap = require('source-map');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// RegEx
+var FILE_EXTENSION_REGEX = /\.(pug|jade)$/;
+var LINE_NUMBER_REGEX = /^;jade_debug_line = ([0-9]+)/;
 
 function pugify(file) {
   var opts = this || {};
 
-  if (!/\.(pug|jade)$/.test(file)) return through();
+  if (!FILE_EXTENSION_REGEX.test(file)) return (0, _through2.default)();
 
   var buffer = '';
 
-  return through(function (chunk) {
+  return (0, _through2.default)(function (chunk) {
     return buffer += chunk.toString();
   }, function end() {
     var _this = this;
 
     // Load config from package.json
-    tools.loadTransformConfig('pugify', file, function (err, data) {
-      if (err) throw new Error('Loading transform config');
+    _browserifyTransformTools2.default.loadTransformConfig('pugify', file, function (err, data) {
+      if (err) throw new Error(err);
 
-      var parsed = undefined;
+      var parsed = undefined,
+          sourceMapped = undefined;
 
       try {
-        parsed = pug.compile(buffer, xtend(opts, data ? data.config : {}));
+        parsed = _pug2.default.compile(buffer, (0, _xtend2.default)(opts, data ? data.config : {}));
+        sourceMapped = generateSourceMaps(buffer, parsed.toString(), file);
       } catch (e) {
         _this.emit('error', e);
         return _this.queue(null);
@@ -34,14 +59,37 @@ function pugify(file) {
         return _this.emit('file', dep);
       });
 
-      _this.queue('var jade=require(\'pug-runtime\');module.exports=' + parsed.toString());
+      _this.queue('var jade=require(\'pug-runtime\');module.exports=' + sourceMapped);
       _this.queue(null);
     });
   });
 }
 
+function generateSourceMaps(src, compiled, file) {
+  var lines = compiled.split('\n');
+  var generator = new _sourceMap.SourceMapGenerator({ file: file + '.js' });
+
+  lines.forEach(function (line, i) {
+    var match = line.match(LINE_NUMBER_REGEX);
+    if (!match || match[1] < 1) return;
+
+    generator.addMapping({
+      source: file,
+      generated: { column: 0, line: i + 2 },
+      original: { column: 0, line: Number(match[1]) }
+    });
+  });
+
+  generator.setSourceContent(file, src);
+
+  var map = _convertSourceMap2.default.fromJSON(generator.toString());
+  lines.push(map.toComment());
+
+  return lines.join('\n');
+}
+
 module.exports = function (opts) {
-  return pugify.bind(xtend({
+  return pugify.bind((0, _xtend2.default)({
     // Default options
     pretty: true,
     compileDebug: true
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 'use strict'
 
-const pug = require('pug')
-const xtend = require('xtend')
-const through = require('through')
-const tools = require('browserify-transform-tools')
+import pug from 'pug'
+import xtend from 'xtend'
+import through from 'through'
+import convert from 'convert-source-map'
+import tools from 'browserify-transform-tools'
+import { SourceMapGenerator } from 'source-map'
+
+// RegEx
+const FILE_EXTENSION_REGEX = /\.(pug|jade)$/
+const LINE_NUMBER_REGEX = /^;jade_debug_line = ([0-9]+)/
 
 function pugify (file) {
   const opts = this || {}
 
-  if (!/\.(pug|jade)$/.test(file)) return through()
+  if (!FILE_EXTENSION_REGEX.test(file)) return through()
 
   let buffer = ''
 
@@ -17,12 +23,13 @@ function pugify (file) {
     function end () {
       // Load config from package.json
       tools.loadTransformConfig('pugify', file, (err, data) => {
-        if (err) throw new Error('Loading transform config')
+        if (err) throw new Error(err)
 
-        let parsed
+        let parsed, sourceMapped
 
         try {
           parsed = pug.compile(buffer, xtend(opts, data ? data.config : {}))
+          sourceMapped = generateSourceMaps(buffer, parsed.toString(), file)
         } catch (e) {
           this.emit('error', e)
           return this.queue(null)
@@ -30,11 +37,34 @@ function pugify (file) {
 
         parsed.dependencies.forEach(dep => this.emit('file', dep))
 
-        this.queue(`var jade=require('pug-runtime');module.exports=${parsed.toString()}`)
+        this.queue(`var jade=require('pug-runtime');module.exports=${sourceMapped}`)
         this.queue(null)
       })
     }
   )
+}
+
+function generateSourceMaps (src, compiled, file) {
+  const lines = compiled.split('\n')
+  const generator = new SourceMapGenerator({ file: `${file}.js` })
+
+  lines.forEach((line, i) => {
+    const match = line.match(LINE_NUMBER_REGEX)
+    if (!match || match[1] < 1) return
+
+    generator.addMapping({
+      source: file,
+      generated: { column: 0, line: i + 2 },
+      original: { column: 0, line: Number(match[1]) }
+    })
+  })
+
+  generator.setSourceContent(file, src)
+
+  const map = convert.fromJSON(generator.toString())
+  lines.push(map.toComment())
+
+  return lines.join('\n')
 }
 
 module.exports = (opts) => pugify.bind(xtend({
