1f373ba72d95bd83c490c1378af7a80f875f4b31
David Willis <dev@davidwillis.co.uk>
2021-03-17 14:41:58 +0000

    Add test for sourcemaps

--- test/bundle.js
+++ test/bundle.js
@@ -1,6 +1,10 @@
 var test = require('tap').test;
 var browserify = require('browserify');
+var fs = require('fs');
 var vm = require('vm');
+var path = require('path');
+var SourceMapConsumer = require('source-map').SourceMapConsumer;
+var convert = require('convert-source-map');
 var pugify = require('../index.js');
 
 test('no options bundle', function(t) {
@@ -24,6 +28,44 @@ test('options bundle', function(t) {
     b.bundle(function (err, src) {
         if (err) t.fail(err);
         testBundle(src, t);
+    });
+});
+
+test('with sourcemap', function (t) {
+
+    async function checkSrcContainsSourceMapFor (src, filename) {
+        var json = convert
+            .fromSource(src.toString('utf8'))
+            .toJSON();
+        var consumer = await new SourceMapConsumer(json);
+        try {
+            var srcFile = path.join(__dirname, '../example', filename);
+            var smSrc = consumer.sourceContentFor(srcFile);
+            var fileSource = await fs.promises.readFile(srcFile, 'utf8');
+            if (smSrc !== fileSource) throw new Error('Sourcemap does not match file content');
+        } finally {
+            consumer.destroy();
+        }
+    }
+
+    t.plan(1);
+    var b = browserify({debug:true});
+    b.add(__dirname + '/../example/bar.js');
+    b.transform(pugify.pug({
+        pretty: true,
+        compileDebug: true
+    }));
+    b.bundle(async function (err, src) {
+        if (err) t.fail(err);
+        try {
+            await Promise.all([
+                checkSrcContainsSourceMapFor(src, 'foo.pug'),
+                checkSrcContainsSourceMapFor(src, 'fooinc.pug'),
+            ]);
+            testBundle(src, t);
+        } catch (err) {
+            t.fail(err);
+        }
     });
 });
 
